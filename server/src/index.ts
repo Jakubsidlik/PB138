c697003aa42b2e854706f6d95c3ddf79f9ea372f
Jakubsidlik <147366291+Jakubsidlik@users.noreply.github.com>
2026-05-26 13:46:29 +0200

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,12 @@ const PORT = env.PORT
 
 app.use(cors())
 app.use(express.json({ limit: '10mb' }))
-app.use(clerkMiddleware())
+app.use(
+  clerkMiddleware({
+    publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+    secretKey: process.env.CLERK_SECRET_KEY,
+  }),
+)
 
 app.use('/api/tasks', tasksRouter)
 app.use('/api/events', eventsRouter)
