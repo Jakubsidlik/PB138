c9d6d6a7c0d8b8b566682bf1c6a9a0ff410a7349
Jakubsidlik <jakubsidlik29@gmail.com>
2026-07-04 01:37:45 +0200

    oprava 3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,7 @@ import { fileURLToPath } from 'url'
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: '/PB138-3/',
+  base: '/PB138/',
   plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
